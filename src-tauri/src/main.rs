aa29d0c56b86a07a7fae714c7b1c282708b0863f
Abhi Arya <abhigyaa@uci.edu>
2024-10-18 18:50:37 -0700

    feat: add env

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,33 +1,4 @@
 #![cfg_attr(
-<<<<<<< HEAD
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
-)]
-
-
-#[tauri::command]
-fn run_subprocess(command: String) -> Result<String, String> {
-    let output = std::process::Command::new(command)
-        .output()
-        .map_err(|e| e.to_string())?;
-
-    if output.status.success() {
-        Ok(String::from_utf8_lossy(&output.stdout).to_string())
-    } else {
-        Err(String::from_utf8_lossy(&output.stderr).to_string())
-    }
-}
-
-
-
-fn main() {
-  let context = tauri::generate_context!();
-  tauri::Builder::default()
-    .menu(tauri::Menu::os_default(&context.package_info().name))
-    .run(context)
-    .expect("error while running tauri application");
-}
-=======
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
   )]
@@ -62,4 +33,3 @@ fn main() {
       .expect("error while running tauri application");
   }
   
->>>>>>> 7791a1b04f24639a4ee6eb5334daca70d6a6292d
