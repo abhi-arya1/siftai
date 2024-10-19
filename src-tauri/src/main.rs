33e675dc5f304d368a6eba07e4aaa39ded80e80f
Abhi Arya <abhigyaa@uci.edu>
2024-10-18 20:10:14 -0700

    add utilieis

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,11 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::api::path::config_dir;
 use tauri::{CustomMenuItem, Menu, Submenu};
 
 mod invokes;
+mod util;
 
 #[tauri::command]
 fn run_subprocess(command: String) -> Result<String, String> {
@@ -17,16 +19,21 @@ async fn gh_oauth() -> Result<String, String> {
     invokes::github_oauth().await
 }
 
-
 fn main() {
     let quit = CustomMenuItem::new("quit".to_string(), "Quit");
     let submenu = Submenu::new("File", Menu::new().add_item(quit));
 
+    let config_path = config_dir() / "sift";
+
+    if let Some(config_path) = config_dir() {
+        // Convert PathBuf to a string and print it
+        println!("{}", config_path.display()); // Use `display()` for PathBuf printing
+    } else {
+        println!("Failed to get config directory");
+    }
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![
-            gh_oauth,
-            run_subprocess
-        ])
+        .invoke_handler(tauri::generate_handler![gh_oauth, run_subprocess])
         .menu(Menu::new().add_submenu(submenu))
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
