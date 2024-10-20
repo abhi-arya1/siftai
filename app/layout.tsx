0c2429befad3c5fdd524acea085e2ecae7ae6d62
Hrishikesh Srihari <rishisrihari9@gmail.com>
2024-10-20 00:59:08 -0700

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="overscroll-none">
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
